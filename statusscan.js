4703eed91148ce33c43f10930c8382f01c024a14
Muh. Shamad SST <shamad2402@gmail.com>
2022-07-09 22:00:07 +0800

    status scan feature add

---
+++ statusscan.js
@@ -0,0 +1,40 @@
+const fs = require('fs');
+const XlsxPopulate = require('xlsx-populate');
+//read scanned files, count frequency
+const allScannedIdBsCount = {};
+const allScannedKecFolder = fs.readdirSync('./output/finish')
+allScannedKecFolder.forEach(kecFolderName => {
+  const allScannedDesaFolderName = fs.readdirSync(`./output/finish/${kecFolderName}`)
+  allScannedDesaFolderName.forEach(scannedDesaFolderName => {
+    const allScannedBSName = fs.readdirSync(`./output/finish/${kecFolderName}/${scannedDesaFolderName}`)
+    allScannedBSName.forEach(bsFileName => {
+      const idBs = bsFileName.replace(/_?\d?\d?\.jpg/, '')
+      if (!allScannedIdBsCount[idBs]) allScannedIdBsCount[idBs] = 1;
+      else allScannedIdBsCount[idBs] ++;
+    })
+  })
+})
+//loop through rows of excel
+const xlsx = require("node-xlsx").default;
+const masterPath = './Kontrol Scan WB.xlsx'
+const masterBSExcel = xlsx.parse(masterPath);
+XlsxPopulate.fromFileAsync(masterPath)
+  .then(workbook => {
+    let sheet = workbook.sheet(0);
+    masterBSExcel[0].data.forEach((row, i) => {
+      if (i > 0) {
+        let idBS = row[5]
+        // console.log(files_id_count[id], id)
+        if (allScannedIdBsCount[idBS]) {
+          sheet.cell("G" + (i+1)).value(allScannedIdBsCount[idBS])
+        } 
+      }
+    })
+    if (fs.existsSync(__dirname + `/Status Scan.xlsx`)) {
+      fs.unlinkSync(__dirname + `/Status Scan.xlsx`);
+    }
+    workbook.toFileAsync(__dirname + `/Status Scan.xlsx`);
+  }).then(dataa => {
+    console.log('Finished');
+  })
+//done
